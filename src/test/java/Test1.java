1faa5f5ab2a155dd390f88dbec609a5861438fde
kemal <kemal182182@gmail.com>
2022-12-03 22:42:35 +0000

    -_- O_o

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,5 +2,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("github repo is already published");
+
+
+        //work
     }
 }
